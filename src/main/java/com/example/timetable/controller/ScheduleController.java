52d1d41d68e00d329a925f3f2bd063694369fa16
jihyeon <bjh20049@naver.com>
2026-04-14 10:36:47 +0900

    feat: 스케쥴 전체 조회 기능 구현

--- src/main/java/com/example/timetable/controller/ScheduleController.java
+++ src/main/java/com/example/timetable/controller/ScheduleController.java
@@ -2,12 +2,13 @@ package com.example.timetable.controller;
 
 import com.example.timetable.dto.CreateScheduleRequest;
 import com.example.timetable.dto.CreateScheduleResponse;
+import com.example.timetable.dto.GetOneScheduleResponse;
 import com.example.timetable.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +18,15 @@ public class ScheduleController {
     @PostMapping("/schedules")
     public CreateScheduleResponse scheduleSave(@RequestBody CreateScheduleRequest request) {
         return scheduleService.scheduleSave(request);
+    }
+
+    @GetMapping("/schedules/{scheduleId}")
+    public GetOneScheduleResponse scheduleGetOne(@PathVariable Long scheduleId) {
+        return scheduleService.getOne(scheduleId);
+    }
+
+    @GetMapping("/schedules")
+    public List<GetOneScheduleResponse> scheduleGetAll() {
+        return scheduleService.getAll();
     }
 }
--- src/main/java/com/example/timetable/service/ScheduleService.java
+++ src/main/java/com/example/timetable/service/ScheduleService.java
@@ -2,11 +2,15 @@ package com.example.timetable.service;
 
 import com.example.timetable.dto.CreateScheduleRequest;
 import com.example.timetable.dto.CreateScheduleResponse;
+import com.example.timetable.dto.GetOneScheduleResponse;
 import com.example.timetable.entity.Schedule;
 import com.example.timetable.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -34,5 +38,44 @@ public class ScheduleService {
                 savedSchedule.getCreatedAt()
         );
     }
+
+
+    //단건 조회
+    @Transactional(readOnly = true)
+    public GetOneScheduleResponse getOne(Long scheduleId) {
+        Schedule schedule = scheduleRepository.findById(scheduleId).orElseThrow(
+                () -> new IllegalStateException("없는 스케쥴입니다.")
+        );
+        return new GetOneScheduleResponse(
+                schedule.getId(),
+                schedule.getTitle(),
+                schedule.getContent(),
+                schedule.getWriter(),
+                schedule.getCreatedAt(),
+                schedule.getModifiedAt()
+        );
+    }
+
+    //전체 조회
+    @Transactional(readOnly = true)
+    public List<GetOneScheduleResponse> getAll() {
+        List<Schedule> schedules = scheduleRepository.findAll();
+
+        List<GetOneScheduleResponse> dtos = new ArrayList<>();
+
+        for (Schedule schedule : schedules) {
+            GetOneScheduleResponse dto = new GetOneScheduleResponse(
+                    schedule.getId(),
+                    schedule.getTitle(),
+                    schedule.getContent(),
+                    schedule.getWriter(),
+                    schedule.getCreatedAt(),
+                    schedule.getModifiedAt()
+            );
+            dtos.add(dto);
+        }
+        return dtos;
+    }
+
 
 }
